de777691184bfc5e161cd5759e689154f16575e9
Bunny899 <prarthit.shah@shoptimize.in>
2021-02-18 11:47:59 +0530

    code updated with hooks approach

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React , {useState,useEffect} from 'react';
 import './App.css';
 import youtube from "./api/youtube";
 import { Row, Col } from 'antd';
 import { SearchBar, VideoList, VideoDetail } from "./components";
-class App extends React.Component {
-  state = {
-    videos: [],
-    selectedVideo: null
-  }
+const App = () =>{
+  // state = {
+  //   videos: [],
+  //   selectedVideo: null
+  // }
+  const [videos,setVideos] = useState([]);
+  const [selectedVideo,setSelectedVideo] = useState(null);
 
-  componentDidMount()
-  {
-    this.handleSubmit('javascript')
-  }
+  // componentDidMount()
+  // {
+  //   this.handleSubmit('javascript')
+  // }
+  useEffect(()=>{
+    handleSubmit('javascript')
+  })
   
-  onVideoSelect = (video) => {
-    this.setState({selectedVideo:video});
+  const onVideoSelect = (video) => {
+    // this.setState({selectedVideo:video});
+    setSelectedVideo(video);
   }
-  handleSubmit = async (searchItem) => {
+  const handleSubmit = async (searchItem) => {
     const response = await youtube.get('search', {
       params: {
         part: 'snippet',
@@ -25,25 +31,25 @@ class App extends React.Component {
         q:searchItem
       }
     });
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0]
-    });
+    // this.setState({
+    //   videos: response.data.items,
+    //   selectedVideo: response.data.items[0]
+    // });
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   }
-  render() {
-    const { selectedVideo,videos } = this.state;
+    // const { selectedVideo,videos } = this.state;
     return (
       <div className="App">
         <Row>
-          <Col span={24}><SearchBar onFormSubmit={this.handleSubmit} /></Col>
+          <Col span={24}><SearchBar onFormSubmit={handleSubmit} /></Col>
         </Row>
         <Row>
           <Col span={16}><VideoDetail video={selectedVideo}/></Col>
-          <Col span={8}><VideoList videos={videos} onVideoSelect={this.onVideoSelect}/></Col>
+          <Col span={8}><VideoList videos={videos} onVideoSelect={onVideoSelect}/></Col>
         </Row>
       </div>
     );
   }
-}
 
 export default App;
--- src/components/SearchBar/Searchbar.js
+++ src/components/SearchBar/Searchbar.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component , useState } from 'react'
 import { Input, Space } from 'antd'
-class Searchbar extends Component {
-    state = {
-        searchItem: ''
-    }
-    handleChange = (event) => {
-        //console.log(event.target.value)
-        this.setState({
-            searchItem: event.target.value
-        })
-    }
-    handleSubmit = (event) => {
-        const { searchItem } = this.state;
+const Searchbar = () =>{
+    const [searchItem,setSearchItems] = useState('');
+    // state = {
+    //     searchItem: ''
+    // }
+    // handleChange = (event) => {
+    //     //console.log(event.target.value)
+    //     this.setState({
+    //         searchItem: event.target.value
+    //     })
+    // }
+    const handleSubmit = (event) => {
+        // const { searchItem } = this.state;
         const { onFormSubmit } = this.props;
         onFormSubmit(searchItem);
         event.preventDefault();
     }
-    render() {
         return (
             <div>
                 <Space>
-                    <form onSubmit={this.handleSubmit}>
-
+                    <form onSubmit={handleSubmit}>
                         <h1 style={{ marginLeft: '40px' }}>Youtube Clone</h1>
-                        <Input size="large" style={{ width: '90%', height: '30px', paddingLeft: '20px' }} placeholder="Search.." onChange={this.handleChange}></Input>
-
+                        <Input size="large" style={{ width: '90%', height: '30px', paddingLeft: '20px' }} placeholder="Search.." onChange={e => setSearchItems(e.target.value)}></Input>
                     </form>
                 </Space>
             </div>
         )
-    }
 }
 
 export default Searchbar;
